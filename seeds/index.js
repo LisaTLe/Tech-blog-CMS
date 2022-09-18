ab4cf96760fe6f6310b7d307331e0213fda9996b
LisaTLe <lisa.le1127@gmail.com>
2022-09-17 22:10:35 -0600

    edit index in seed

--- seeds/index.js
+++ seeds/index.js
@@ -1,4 +1,7 @@
 const sequelize = require("../config/connection");
+const User = require("../models/User");
+const Post = require("../models/Post");
+const Comment = require("../models/Comment");
 
 const seedUser = require("../seeds/user-seed");
 const seedPost = require("../seeds/post-seed");
@@ -7,9 +10,9 @@ const seedComment = require("../seeds/comment-seed");
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
-  await seedUser();
-  await seedPost();
-  await seedComment();
+  await User.bulkCreate(seedUser, { individualHooks: true });
+  await Post.bulkCreate(seedPost);
+  await Comment.bulkCreate(seedComment);
 
   process.exit(0);
 };
