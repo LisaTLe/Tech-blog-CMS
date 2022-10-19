6d93b095d2972d59d3969cffc46ce7c5b9151700
LisaTLe <lisa.le1127@gmail.com>
2022-10-18 21:08:44 -0600

    uncomment requests

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -4,66 +4,66 @@ const sequelize = require("../../config/connection");
 const withAuth = require("../../utils/auth");
 
 //GET request for all posts
-// router.get("/", (req, res) => {
-//   Post.findAll({
-//     attributes: ["id", "title", "post_content", "created_at"],
-//     order: [["created_at", "DESC"]],
-//     include: [
-//       {
-//         model: Comment,
-//         attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
-//         include: {
-//           model: User,
-//           attributes: ["username", "github"],
-//         },
-//       },
-//       {
-//         model: User,
-//         attributes: ["username", "github"],
-//       },
-//     ],
-//   })
-//     .then((dbPostData) => res.json(dbPostData))
-//     .catch((err) => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
-// });
+router.get("/", (req, res) => {
+  Post.findAll({
+    attributes: ["id", "title", "post_content", "created_at"],
+    order: [["created_at", "DESC"]],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username", "github"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username", "github"],
+      },
+    ],
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 //GET request to find one post
-// router.get("/:id", (req, res) => {
-//   Post.findOne({
-//     where: {
-//       id: req.params.id,
-//     },
-//     attributes: ["id", "title", "post_content", "created_at"],
-//     include: [
-//       {
-//         model: User,
-//         attributes: ["username", "github"],
-//       },
-//       {
-//         model: Comment,
-//         attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
-//         include: {
-//           model: User,
-//           attributes: ["username", "github"],
-//         },
-//       },
-//     ],
-//   })
-//     .then((dbPostData) => {
-//       if (!dbPostData) {
-//         res.status(404).json({ message: "Post cannot be found with this id" });
-//         return;
-//       }
-//       res.json(dbPostData);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
-// });
+router.get("/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "title", "post_content", "created_at"],
+    include: [
+      {
+        model: User,
+        attributes: ["username", "github"],
+      },
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username", "github"],
+        },
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "Post cannot be found with this id" });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 //POST request for post
 router.post("/", withAuth, async (req, res) => {
